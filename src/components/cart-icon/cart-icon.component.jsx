47b4f4854792384829860cf2f8547c589ed3bc80
akmalelahi <akmalelahishaikh@gmail.com>
2020-02-01 01:12:08 +0500

    updated application with reselect, replace cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,16 +4,21 @@ import { toggleCartHidden } from '../../redux/cart/cart-actions'
 
 import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
+import { selectCartItemsCount } from '../../redux/cart/cart-selectors';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, cartItemsCount }) => (
     <div className="cart-icon">
         <ShoppingBag className="shopping-icon" onClick={toggleCartHidden} />
-        <span className="item-count">0</span>
+        <span className="item-count">{cartItemsCount}</span>
     </div>
 )
+
+const mapStateToProps = state => ({
+    cartItemsCount: selectCartItemsCount(state)
+})
 const mapDispatchToprops = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
 
-export default connect(null, mapDispatchToprops)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToprops)(CartIcon)
--- src/components/cartDropdown/cart-dropdown.component.jsx
+++ src/components/cartDropdown/cart-dropdown.component.jsx
@@ -5,16 +5,17 @@ import CustomButton from '../custom-botton/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart-selectors';
 
 const CartDropDown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
+            {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
         </div>
-        {cartItems.map(cartItem =><CartItem key={cartItem.id} item={cartItem}/>)}
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems : selectCartItems(state)
 })
 export default connect(mapStateToProps)(CartDropDown);
